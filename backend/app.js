fe79d853d92456f2208478a47ab9a5c71abcea1f
velu murugesan <velu2092002@gmail.com>
2025-02-04 09:42:05 +0530

    commited

---
+++ backend/app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const userRouter = require("./routes/userRouter");
+const errorHandler = require("./middlewares/errorHandlerMiddleware");
+const categoryRouter = require("./routes/categoryRouter");
+const transactionRouter = require("./routes/transactionRouter");
+const app = express();
+
+//!Connect to mongodb
+mongoose
+  .connect("mongodb+srv://velu2092002:48XAbRzSmnlGjHCI@cluster0.68tpt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  .then(() => console.log("DB Connected"))
+  .catch((e) => console.log(e));
+
+//! Cors config
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+};
+app.use(cors(corsOptions));
+//!Middlewares
+app.use(express.json()); //?Pass incoming json data
+//!Routes
+app.use("/", userRouter);
+app.use("/", categoryRouter);
+app.use("/", transactionRouter);
+//! Error
+app.use(errorHandler);
+
+//!Start the server
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () =>
+  console.log(`Server is running on this port... ${PORT} `)
+);
+
+
---
+++ frontend/src/main.jsx
@@ -0,0 +1,25 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import React from 'react'
+
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { store } from "./redux/store/store.js";
+
+const client = new QueryClient();
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Provider store={store}>
+      <QueryClientProvider client={client}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </Provider>
+  </StrictMode>
+);
